32176c5a767f007684e8e66d80dff14599502347
HamadZafarullah <hamadzafarullah@gmail.com>
2022-11-07 16:42:59 -0500

    done

---
+++ src/Hello.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+
+function Hello(props){
+
+    return(
+       <div>
+        <h1>"Hello World"</h1>
+        <h1>"hello, {props.name}</h1>
+       </div> 
+    )
+}
+
+
+export default Hello
